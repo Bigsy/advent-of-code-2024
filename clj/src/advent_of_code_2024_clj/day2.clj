88dd0f60de23ec004cd0536ba7ff3c49c7b062e5
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-02 17:23:01 +0000

    day2

---
+++ clj/src/advent_of_code_2024_clj/day2.clj
@@ -0,0 +1,14 @@
+(ns advent-of-code-2024-clj.day2
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(defn parse-input [input]
+  (->> (str/split-lines input)))
+
+(defn solve-part1 [input]
+  (let [data (parse-input input)]
+    0))
+
+(defn solve-part2 [input]
+  (let [data (parse-input input)]
+    0))

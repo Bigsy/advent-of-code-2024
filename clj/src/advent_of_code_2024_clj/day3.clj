ad0acb6b63e48499c91941536c716186fe106b74
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-03 17:42:47 +0000

    day 3 clj

--- clj/src/advent_of_code_2024_clj/day3.clj
+++ clj/src/advent_of_code_2024_clj/day3.clj
@@ -12,35 +12,32 @@
                    (parse-long y))))
          (reduce + 0))))
 
-(defn solve-part2 [input]
+ (defn solve-part2 [input]
   (let [command-pattern #"(?:mul\((\d+),(\d+)\)|(?:(do|don't)\(\)))"
-        find-commands (fn [pattern]
-                       (loop [matcher (re-matcher pattern input)
-                              commands []]
-                         (if (.find matcher)
-                           (let [[_ x y cmd] (re-groups matcher)]
-                             (recur 
-                              matcher
-                              (conj commands
-                                    {:pos (.start matcher)
-                                     :cmd (cond
-                                           x [:mul (parse-long x) (parse-long y)]
-                                           cmd [(if (= cmd "do") :enable :disable)])})))
-                           commands)))]
-    (->> (find-commands command-pattern)
-         (sort-by :pos)
-         (reduce (fn [{:keys [enabled? sum]} {:keys [cmd]}]
-                  (let [[op & args] cmd]
-                    (case op
-                      :mul {:enabled? enabled? 
-                           :sum (if enabled? 
-                                 (+ sum (apply * args))
-                                 sum)}
-                      :enable {:enabled? true :sum sum}
-                      :disable {:enabled? false :sum sum})))
-                {:enabled? true :sum 0})
-         :sum)))
-
+        commands (->> (loop [matcher (re-matcher command-pattern input)
+                           result []]
+                      (if (.find matcher)
+                        (let [[_ x y cmd] (re-groups matcher)]
+                          (recur matcher
+                                (conj result
+                                      {:pos (.start matcher)
+                                       :cmd (if x
+                                             [:mul (parse-long x) (parse-long y)]
+                                             [(if (= cmd "do") :enable :disable)])})))
+                        result))
+                     (sort-by :pos))]
+    (:sum
+     (reduce (fn [{:keys [enabled? sum]} {:keys [cmd]}]
+              (let [[op & args] cmd]
+                (case op
+                  :mul {:enabled? enabled?
+                       :sum (if enabled?
+                             (+ sum (apply * args))
+                             sum)}
+                  :enable {:enabled? true :sum sum}
+                  :disable {:enabled? false :sum sum})))
+            {:enabled? true :sum 0}
+            commands))))
 (comment
   (solve-part1 (slurp (clojure.java.io/resource "day3.txt"))) ;; 167650499
   (solve-part2 (slurp (clojure.java.io/resource "day3.txt"))) ;; 95846796

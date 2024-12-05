a96f2566416e08df243ceb9a9ddee3eed570f055
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-05 19:50:46 +0000

    day 5 part 2

--- clj/src/advent_of_code_2024_clj/day5.clj
+++ clj/src/advent_of_code_2024_clj/day5.clj
@@ -35,9 +35,43 @@
          (map middle-number)
          (reduce +))))
 
+(defn sort-by-rules [rules pages]
+  (let [page-set (set pages)
+        relevant-rules (filter (fn [[before after]]
+                               (and (contains? page-set before)
+                                    (contains? page-set after)))
+                             rules)]
+    (loop [current-pages (vec pages)]
+      (let [needs-swap? (some (fn [[before after]]
+                               (let [before-idx (.indexOf current-pages before)
+                                     after-idx (.indexOf current-pages after)]
+                                 (and (>= before-idx 0)
+                                      (>= after-idx 0)
+                                      (> before-idx after-idx))))
+                             relevant-rules)]
+        (if needs-swap?
+          (let [[before after] (first (filter (fn [[before after]]
+                                              (let [before-idx (.indexOf current-pages before)
+                                                    after-idx (.indexOf current-pages after)]
+                                                (and (>= before-idx 0)
+                                                     (>= after-idx 0)
+                                                     (> before-idx after-idx))))
+                                            relevant-rules))
+                before-idx (.indexOf current-pages before)
+                after-idx (.indexOf current-pages after)]
+            (recur (assoc current-pages
+                         after-idx before
+                         before-idx after)))
+          current-pages)))))
+
 (defn solve-part2 [input]
-  )
+  (let [{:keys [rules updates]} (parse-input input)]
+    (->> updates
+         (filter #(not (valid-order? rules %)))
+         (map #(sort-by-rules rules %))
+         (map middle-number)
+         (reduce +))))
 
 (comment
-  (solve-part1 (slurp (clojure.java.io/resource "day5.txt"))) ;; 
+  (solve-part1 (slurp (clojure.java.io/resource "day5.txt"))) ;; 5639
   (solve-part2 (slurp (clojure.java.io/resource "day5.txt")))) ;;

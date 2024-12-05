fa387b4c1685ff7ac9bf103dd94155dcb44b27f2
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-05 19:54:04 +0000

    tidy up

--- clj/src/advent_of_code_2024_clj/day5.clj
+++ clj/src/advent_of_code_2024_clj/day5.clj
@@ -11,18 +11,16 @@
   (mapv parse-long (str/split update #",")))
 
 (defn parse-input [input]
-  (let [[rules-section updates-section] (str/split input #"\n\n")
-        rules (map parse-rule (str/split-lines rules-section))
-        updates (map parse-update (str/split-lines updates-section))]
-    {:rules rules :updates updates}))
+  (let [[rules-section updates-section] (str/split input #"\n\n")]
+    {:rules (map parse-rule (str/split-lines rules-section))
+     :updates (map parse-update (str/split-lines updates-section))}))
 
 (defn valid-order? [rules pages]
   (let [page-indices (into {} (map-indexed (fn [idx page] [page idx]) pages))]
     (every? (fn [[before after]]
-              (or 
-               (not (contains? page-indices before))
-               (not (contains? page-indices after))
-               (< (get page-indices before) (get page-indices after))))
+              (or (not (contains? page-indices before))
+                  (not (contains? page-indices after))
+                  (< (get page-indices before) (get page-indices after))))
             rules)))
 
 (defn middle-number [nums]
@@ -31,9 +29,19 @@
 (defn solve-part1 [input]
   (let [{:keys [rules updates]} (parse-input input)]
     (->> updates
-         (filter #(valid-order? rules %))
-         (map middle-number)
+         (keep #(when (valid-order? rules %) (middle-number %)))
          (reduce +))))
+
+(defn swap-positions [pages [before after]]
+  (let [before-idx (.indexOf pages before)
+        after-idx (.indexOf pages after)]
+    (if (and (>= before-idx 0)
+             (>= after-idx 0)
+             (> before-idx after-idx))
+      (assoc pages
+             after-idx before
+             before-idx after)
+      pages)))
 
 (defn sort-by-rules [rules pages]
   (let [page-set (set pages)
@@ -42,36 +50,18 @@
                                     (contains? page-set after)))
                              rules)]
     (loop [current-pages (vec pages)]
-      (let [needs-swap? (some (fn [[before after]]
-                               (let [before-idx (.indexOf current-pages before)
-                                     after-idx (.indexOf current-pages after)]
-                                 (and (>= before-idx 0)
-                                      (>= after-idx 0)
-                                      (> before-idx after-idx))))
-                             relevant-rules)]
-        (if needs-swap?
-          (let [[before after] (first (filter (fn [[before after]]
-                                              (let [before-idx (.indexOf current-pages before)
-                                                    after-idx (.indexOf current-pages after)]
-                                                (and (>= before-idx 0)
-                                                     (>= after-idx 0)
-                                                     (> before-idx after-idx))))
-                                            relevant-rules))
-                before-idx (.indexOf current-pages before)
-                after-idx (.indexOf current-pages after)]
-            (recur (assoc current-pages
-                         after-idx before
-                         before-idx after)))
-          current-pages)))))
+      (let [next-pages (reduce swap-positions current-pages relevant-rules)]
+        (if (= next-pages current-pages)
+          current-pages
+          (recur next-pages))))))
 
 (defn solve-part2 [input]
   (let [{:keys [rules updates]} (parse-input input)]
     (->> updates
-         (filter #(not (valid-order? rules %)))
-         (map #(sort-by-rules rules %))
-         (map middle-number)
+         (keep #(when-not (valid-order? rules %)
+                 (middle-number (sort-by-rules rules %))))
          (reduce +))))
 
 (comment
   (solve-part1 (slurp (clojure.java.io/resource "day5.txt"))) ;; 5639
-  (solve-part2 (slurp (clojure.java.io/resource "day5.txt")))) ;;
+  (solve-part2 (slurp (clojure.java.io/resource "day5.txt")))) ;; 5273

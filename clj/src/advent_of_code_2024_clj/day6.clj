5d5c52c06236dcf081c4c34cd120552fd7baf64f
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-06 21:37:10 +0000

    day6

--- clj/src/advent_of_code_2024_clj/day6.clj
+++ clj/src/advent_of_code_2024_clj/day6.clj
@@ -49,8 +49,35 @@
             (recur pos (turn-right dir) visited)
             (recur next-p dir (conj visited next-p))))))))
 
+(defn simulate-path [{:keys [grid width height] :as state} start-pos extra-obstacle]
+  (loop [pos start-pos
+         dir :up
+         path []
+         visited #{}]
+    (let [dir-vec (directions dir)
+          next-p (next-pos pos dir-vec)]
+      (cond
+        (not (in-bounds? state next-p)) nil
+        (or (grid next-p) (= next-p extra-obstacle))
+        (let [new-pos pos
+              new-dir (turn-right dir)
+              new-path (conj path [new-pos dir])]
+          (if (visited [new-pos new-dir])
+            (drop-while #(not= % [new-pos new-dir]) new-path)
+            (recur new-pos new-dir new-path (conj visited [new-pos new-dir]))))
+        :else
+        (recur next-p dir (conj path [pos dir]) (conj visited [pos dir]))))))
+
 (defn solve-part2 [input]
-  )
+  (let [{:keys [grid width height start-pos] :as state} (parse-grid input)]
+    (->> (for [y (range height)
+               x (range width)
+               :let [pos [y x]]
+               :when (and (not= pos start-pos)
+                         (not (grid pos)))]
+           pos)
+         (filter #(simulate-path state start-pos %))
+         count)))
 
 (comment
   (solve-part1 (slurp (clojure.java.io/resource "day6.txt"))) ;;4515

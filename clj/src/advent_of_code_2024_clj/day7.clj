bdfdad7dd9b59917ef759269a063136997b60cec
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-07 19:08:47 +0000

    day 7

--- clj/src/advent_of_code_2024_clj/day7.clj
+++ clj/src/advent_of_code_2024_clj/day7.clj
@@ -16,6 +16,22 @@
     (first nums)
     (map vector (rest nums) operators)))
 
+(defn evaluate-with-concat [nums operators]
+  (loop [result (first nums)
+         remaining-nums (rest nums)
+         remaining-ops operators]
+    (if (empty? remaining-nums)
+      result
+      (let [next-num (first remaining-nums)
+            op (first remaining-ops)]
+        (recur
+          (case op
+            \+ (+ result next-num)
+            \* (* result next-num)
+            \| (parse-long (str result next-num)))  ; Concatenation operator
+          (rest remaining-nums)
+          (rest remaining-ops))))))
+
 (defn generate-operator-combinations [n]
   (if (zero? n)
     [[]]
@@ -23,10 +39,22 @@
           op [\+ \*]]
       (conj combo op))))
 
+(defn generate-operator-combinations-with-concat [n]
+  (if (zero? n)
+    [[]]
+    (for [combo (generate-operator-combinations-with-concat (dec n))
+          op [\+ \* \|]]  ; Add concatenation operator
+      (conj combo op))))
+
 (defn can-equation-be-solved? [[target nums]]
   (let [operator-count (dec (count nums))
         operator-combinations (generate-operator-combinations operator-count)]
     (some #(= target (evaluate nums %)) operator-combinations)))
+
+(defn can-equation-be-solved-part2? [[target nums]]
+  (let [operator-count (dec (count nums))
+        operator-combinations (generate-operator-combinations-with-concat operator-count)]
+    (some #(= target (evaluate-with-concat nums %)) operator-combinations)))
 
 (defn solve-part1 [input]
   (->> input
@@ -37,8 +65,13 @@
        (reduce +)))
 
 (defn solve-part2 [input]
-  )
+  (->> input
+       str/split-lines
+       (map parse-line)
+       (filter can-equation-be-solved-part2?)
+       (map first)
+       (reduce +)))
 
 (comment
   (solve-part1 (slurp (clojure.java.io/resource "day7.txt"))) ;;14711933466277
-  (solve-part2 (slurp (clojure.java.io/resource "day7.txt")))) ;;1309
+  (solve-part2 (slurp (clojure.java.io/resource "day7.txt")))) ;;286580387663654
---
+++ elixir/lib/day7.ex
@@ -0,0 +1,11 @@
+defmodule Day7 do
+
+
+  def solve_part1(input) do
+
+  end
+
+  def solve_part2(input) do
+
+  end
+end

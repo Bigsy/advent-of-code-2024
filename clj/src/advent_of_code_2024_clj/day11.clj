4c6f4d4ecb43f0725acb39bd1bd4424f1ca6e544
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-12 12:46:53 +0000

    day11

--- clj/src/advent_of_code_2024_clj/day11.clj
+++ clj/src/advent_of_code_2024_clj/day11.clj
@@ -1,14 +1,33 @@
 (ns advent-of-code-2024-clj.day11
   (:require
-    [hashp.core]
     [clojure.string :as str]))
 
+(defn transform-stone [stone]
+  (cond
+    (zero? stone) [1]
+    (even? (count (str stone))) [(Long/parseLong (subs (str stone) 0 (/ (count (str stone)) 2)))
+                                (Long/parseLong (subs (str stone) (/ (count (str stone)) 2)))]
+    :else [(* stone 2024)]))
+
+(defn blink [stones]
+  (->> stones
+       (mapcat transform-stone)))
+
 (defn solve-part1 [input]
-  )
+  (let [initial-stones (->> (str/split input #" ")
+                           (map #(Integer/parseInt %)))]
+    (->> (iterate blink initial-stones)
+         (drop 25)
+         first
+         count)))
 
 (defn solve-part2 [input]
   )
 
+(defn -main []
+  (let [input (str/trim (slurp "resources/day11.txt"))]
+    (println "Part 2:" (solve-part2 input))))
+
 (comment
-  (solve-part1 (str/trim (slurp (clojure.java.io/resource "day10.txt")))) ;; 
-  (solve-part2 (str/trim (slurp (clojure.java.io/resource "day10.txt"))))) ;; 
+  (solve-part1 (str/trim (slurp (clojure.java.io/resource "day11.txt")))) ;; 217443
+  (solve-part2 (str/trim (slurp (clojure.java.io/resource "day11.txt")))))

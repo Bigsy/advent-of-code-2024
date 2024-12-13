a6acb18676699627e835ae41d15da208e73a1ccf
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-13 19:45:24 +0000

    day13

--- clj/src/advent_of_code_2024_clj/day13.clj
+++ clj/src/advent_of_code_2024_clj/day13.clj
@@ -3,12 +3,35 @@
     [hashp.core]
     [clojure.string :as str]))
 
+(defn parse-line [line]
+  (let [[_ ax ay bx by px py] (re-matches #"Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)" line)]
+    {:a {:x (parse-long ax) :y (parse-long ay)}
+     :b {:x (parse-long bx) :y (parse-long by)}
+     :prize {:x (parse-long px) :y (parse-long py)}}))
+
+(defn parse-input [input]
+  (->> (str/split input #"\n\n")
+       (map parse-line)))
+
+(defn can-reach-prize? [{:keys [a b prize]}]
+  (for [a-presses (range 101)
+        b-presses (range 101)
+        :let [x-pos (+ (* a-presses (:x a)) (* b-presses (:x b)))
+              y-pos (+ (* a-presses (:y a)) (* b-presses (:y b)))
+              tokens (+ (* 3 a-presses) b-presses)]
+        :when (and (= x-pos (:x prize))
+                  (= y-pos (:y prize)))]
+    tokens))
+
 (defn solve-part1 [input]
-)
+  (->> (parse-input input)
+       (keep #(when-let [solutions (seq (can-reach-prize? %))]
+                (apply min solutions)))
+       (reduce +)))
 
 (defn solve-part2 [input]
   )
 
 (comment
-  (solve-part1 (str/trim (slurp (clojure.java.io/resource "day13.txt")))) 
-  (solve-part2 (str/trim (slurp (clojure.java.io/resource "day13.txt"))))) 
+  (solve-part1 (str/trim (slurp (clojure.java.io/resource "day13.txt")))) ;; 39748 
+  (solve-part2 (str/trim (slurp (clojure.java.io/resource "day13.txt")))))

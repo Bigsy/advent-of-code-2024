5619d21f4eb6b868106e8ed5b41a463722405da8
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-16 10:59:05 +0000

    day16

---
+++ clj/src/advent_of_code_2024_clj/day16.clj
@@ -0,0 +1,14 @@
+(ns advent-of-code-2024-clj.day16
+  (:require
+   [clojure.java.io :as io]
+   [hashp.core]
+   [clojure.string :as str]))
+
+
+(defn solve-part1 [input])
+
+(defn solve-part2 [input])
+
+(comment
+  (solve-part1 (str/trim (slurp (io/resource "day16.txt")))) ;;
+  (solve-part2 (str/trim (slurp (io/resource "day16.txt"))))) ;; 

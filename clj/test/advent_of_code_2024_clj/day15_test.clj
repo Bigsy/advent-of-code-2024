0b2225527c7e9be1293f3bc8c82fb297f2b0fa28
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-19 13:38:02 +0000

    day19

--- clj/test/advent_of_code_2024_clj/day15_test.clj
+++ clj/test/advent_of_code_2024_clj/day15_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2024-clj.day15 :refer [solve-part1]]))
 
-(def example-input "########
+(def example-input1 "########
 #..O.O.#
 ##@.O..#
 #...O..#
@@ -13,6 +13,28 @@
 
 <^^>>>vv<v>>v<<")
 
+(def example-input2 "##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^")
+
 (deftest part1-test
   (testing "Example input gives correct result"
-    (is (= 2028 (solve-part1 example-input)))))
+    (is (= 10092 (solve-part1 example-input1)))))

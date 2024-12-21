910c33e7337f8c3857d648c75a8255e3b17a5445
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-21 19:45:41 +0000

    day21

--- clj/src/advent_of_code_2024_clj/day21.clj
+++ clj/src/advent_of_code_2024_clj/day21.clj
@@ -4,19 +4,8 @@
    [hashp.core]
    [clojure.string :as str]))
 
-
-(defn some-bfs [code]
-  ; Compute or approximate the shortest button-press sequence length...
-  42) ; Placeholder
-
 (defn solve-part1 [input]
-  (let [lines (clojure.string/split-lines input)]
-    (->> lines
-         (map (fn [line]
-                (let [num (Integer. (re-find #"\d+" line))
-                      shortest-len (some-bfs line)]
-                  (* num shortest-len))))
-         (reduce +))))
+  )
 
 (defn solve-part2 [input]
   )

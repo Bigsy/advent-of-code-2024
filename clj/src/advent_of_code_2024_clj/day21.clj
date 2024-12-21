100925006e527bdb3b021887381765f9e51dce38
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-21 19:44:51 +0000

    day21

--- clj/src/advent_of_code_2024_clj/day21.clj
+++ clj/src/advent_of_code_2024_clj/day21.clj
@@ -5,12 +5,22 @@
    [clojure.string :as str]))
 
 
+(defn some-bfs [code]
+  ; Compute or approximate the shortest button-press sequence length...
+  42) ; Placeholder
+
 (defn solve-part1 [input]
-  )
+  (let [lines (clojure.string/split-lines input)]
+    (->> lines
+         (map (fn [line]
+                (let [num (Integer. (re-find #"\d+" line))
+                      shortest-len (some-bfs line)]
+                  (* num shortest-len))))
+         (reduce +))))
 
 (defn solve-part2 [input]
   )
 
 (comment
-  (solve-part1 (str/trim (slurp (io/resource "day21.txt")))) ;; 
-  (solve-part2 (str/trim (slurp (io/resource "day21.txt"))))) ;; 22068 low
+  (solve-part1 (str/trim (slurp (io/resource "day21.txt")))) ;; 142884 low
+  (solve-part2 (str/trim (slurp (io/resource "day21.txt"))))) ;;

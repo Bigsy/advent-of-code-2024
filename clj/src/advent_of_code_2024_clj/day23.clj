a8895386a4b4082742822569a5c8e7b8f02c001e
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-23 11:06:12 +0000

    day23

--- clj/src/advent_of_code_2024_clj/day23.clj
+++ clj/src/advent_of_code_2024_clj/day23.clj
@@ -2,7 +2,8 @@
   (:require
    [clojure.java.io :as io]
    [hashp.core]
-   [clojure.string :as str]))
+   [clojure.string :as str]
+   [clojure.math.combinatorics :as combo]))
 
 (defn parse-connections [input]
   (->> (str/split-lines input)
@@ -37,9 +38,45 @@
        (filter has-t-computer?)
        count))
 
+(defn fully-connected? [graph computers]
+  (every? (fn [[a b]]
+            (contains? (get graph a) b))
+          (for [a computers
+                b computers
+                :when (not= a b)]
+            [a b])))
+
+(defn bron-kerbosch
+  ([graph] (bron-kerbosch graph #{} (set (keys graph)) #{}))
+  ([graph r p x]
+   (if (and (empty? p) (empty? x))
+     [r]
+     (let [pivot (first (or (seq p) (seq x)))
+           nodes (if pivot
+                  (remove #(contains? (get graph pivot) %) p)
+                  p)]
+       (mapcat
+        (fn [v]
+          (let [neighbors (get graph v)]
+            (bron-kerbosch
+             graph
+             (conj r v)
+             (set (filter neighbors p))
+             (set (filter neighbors x)))))
+        nodes)))))
+
+(defn find-largest-clique [graph]
+  (->> (bron-kerbosch graph)
+       (sort-by count >)
+       first))
+
 (defn solve-part2 [input]
-  )
+  (->> input
+       parse-connections
+       find-largest-clique
+       sort
+       (str/join ",")))
 
 (comment
   (solve-part1 (str/trim (slurp (io/resource "day23.txt")))) ;; 1083
-  (solve-part2 (str/trim (slurp (io/resource "day23.txt"))))) ;;
+  (solve-part2 (str/trim (slurp (io/resource "day23.txt"))))) ;; "as,bu,cp,dj,ez,fd,hu,it,kj,nx,pp,xh,yu"

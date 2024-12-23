fc57c0d4b7134a48a972eab70db40655cea0eeea
Bill Hedworth <bill.hedworth@gmail.com>
2024-12-23 10:32:49 +0000

    day23

--- clj/src/advent_of_code_2024_clj/day23.clj
+++ clj/src/advent_of_code_2024_clj/day23.clj
@@ -4,12 +4,42 @@
    [hashp.core]
    [clojure.string :as str]))
 
+(defn parse-connections [input]
+  (->> (str/split-lines input)
+       (reduce (fn [acc line]
+                 (let [[a b] (str/split line #"-")]
+                   (-> acc
+                       (update a (fnil conj #{}) b)
+                       (update b (fnil conj #{}) a))))
+               {})))
+
+(defn find-connected-triples [graph]
+  (let [nodes (keys graph)]
+    (->> (for [a nodes
+               b nodes
+               c nodes
+               :when (and (not= a b)
+                         (not= b c)
+                         (not= a c)
+                         (contains? (get graph a) b)
+                         (contains? (get graph b) c)
+                         (contains? (get graph a) c))]
+           #{a b c})
+         (into #{}))))
+
+(defn has-t-computer? [triple]
+  (some #(str/starts-with? % "t") triple))
+
 (defn solve-part1 [input]
-  )
+  (->> input
+       parse-connections
+       find-connected-triples
+       (filter has-t-computer?)
+       count))
 
 (defn solve-part2 [input]
   )
 
 (comment
-  (solve-part1 (str/trim (slurp (io/resource "day23.txt")))) ;;
+  (solve-part1 (str/trim (slurp (io/resource "day23.txt")))) ;; 1083
   (solve-part2 (str/trim (slurp (io/resource "day23.txt"))))) ;;
